e693aea149598e9bbacce9549d5fea224a42e79e
Gukjin Kim <exodus0315@outlook.com>
2022-02-17 23:43:31 +0900

    upgrade todolist

--- main.js
+++ main.js
@@ -6,3 +6,16 @@
 5. 별표 모양을 눌러 중요 표시를 설정할 수 있다.
 6. 특정 탭을 클릭하면 해당 탭으로 이동한 뒤 목록을 표시한다. (전체, 중요, 완료, 미완료)
 */
+
+let userInput = document.getElementById("user-input");
+let inputButton = document.getElementById("input-button");
+
+inputButton.addEventListener("click", inputTask);
+userInput.addEventListener("click", function () {
+  userInput.value = "";
+});
+
+function inputTask() {
+  newTask = userInput.value;
+  console.log(newTask);
+}
