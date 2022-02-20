cdfed865b9a6038b991278a505f83cf5ac46fcd4
Gukjin Kim <exodus0315@outlook.com>
2022-02-20 22:28:33 +0900

    develop 20220220
    Read readme

--- main.js
+++ main.js
@@ -10,16 +10,36 @@
 let userInput = document.getElementById("user-input");
 let inputButton = document.getElementById("input-button");
 let checkButton = document.getElementById("check-button");
-let tabs = document.querySelectorAll("nav-item");
-let selectedTab = "all";
-let taskList = [];
-let checkedList = [];
+let tabs = document.querySelectorAll(".nav-item button");
+let pageText = document.querySelectorAll(".task p");
+let selectedTab = "tab-all";
+let taskList = []; // tab-all일때 사용할 배열
+let checkedList = []; //tab-all이 아닐때 사용할 배열
+
+// 모달창에 사용
+const modal = document.querySelector(".modal");
+const closeButton = modal.querySelector("button");
+const modalBackground = modal.querySelector(".modal-background");
+
+closeButton.addEventListener("click", displayModal);
+modalBackground.addEventListener("click", displayModal);
+
+window.document.onkeydown = function (event) {
+  if (event.keyCode == 27) {
+    displayModal();
+  }
+};
 
 inputButton.addEventListener("click", addTask);
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filtering(event);
+  });
+}
 
 function addTask() {
   if (userInput.value == "") {
-    console.log("내용을 입력해주세요.");
+    displayModal();
   } else {
     let task = {
       id: taskList.length,
@@ -35,15 +55,29 @@ function addTask() {
 
 function render() {
   let addHTML = "";
+
   let list = [];
-  if (selectedTab == "all") {
+  if (selectedTab == "tab-all") {
     list = taskList;
   } else {
     list = checkedList;
   }
 
-  for (i = 0; i < taskList.length; i++) {
-    addHTML += `<div class="task">
+  for (i = 0; i < list.length; i++) {
+    if (list[i].isComplete) {
+      addHTML += `<div class="task" style="background-color: #0B6138">
+  <p style="color: #ffffff">${list[i].taskContent}</p>
+  <div class="task-button">
+    <button id="check-button" onclick="checkTask(${list[i].id})">
+      <i class="fa-solid fa-calendar-check fa-xl"></i>
+    </button>
+    <button id="delete-button" onclick="deleteTask(${list[i].id})">
+      <i class="fa-solid fa-calendar-minus fa-xl"></i>
+    </button>
+  </div>
+</div>`;
+    } else {
+      addHTML += `<div class="task">
   <p>${list[i].taskContent}</p>
   <div class="task-button">
     <button id="check-button" onclick="checkTask(${list[i].id})">
@@ -54,6 +88,7 @@ function render() {
     </button>
   </div>
 </div>`;
+    }
   }
 
   document.getElementById("task-area").innerHTML = addHTML;
@@ -70,7 +105,7 @@ function checkTask(id) {
       console.log(taskList[i].isComplete);
     }
   }
-  render();
+  filtering();
 }
 
 function deleteTask(id) {
@@ -79,5 +114,49 @@ function deleteTask(id) {
       taskList.splice(i, 1);
     }
   }
+  filtering();
+}
+
+function filtering(event) {
+  if (event) {
+    selectedTab = event.target.id;
+    console.log(selectedTab);
+  }
+
+  checkedList = [];
+  if (selectedTab === "tab-notDone") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        checkedList.push(taskList[i]);
+      }
+    }
+  } else if (selectedTab === "tab-done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete) {
+        checkedList.push(taskList[i]);
+      }
+    }
+  }
   render();
+}
+
+// function modalWindow() {
+//   modal.classList.toggle("show");
+//   if (modal.classList.contains("show")) {
+//     body.style.overflow = "hidden";
+//   }
+
+//   modal.addEventListener("click", (event) => {
+//     if (event.target === modal) {
+//       modal.classList.toggle("show");
+
+//       if (!modal.classList.contains("show")) {
+//         body.style.overflow = "auto";
+//       }
+//     }
+//   });
+// }
+
+function displayModal() {
+  modal.classList.toggle("hidden");
 }
