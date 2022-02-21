f9031a62aea5be5ce203fc272f49ffe105eba1d0
Gukjin Kim <exodus0315@outlook.com>
2022-02-21 22:53:01 +0900

    delete clg
    delete console.log & comment

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-/*
-1. 유저가 투두를 입력한다 
-2. 추가 버튼을 눌러 목록에 추가한다. (키보드 엔터키 사용시 추가)
-3. Check 버튼을 눌러 할일 완료 / 미완료 수정한다.
-4. Delete 버튼을 눌러 목록을 삭제할 수 있다.
-5. 별표 모양을 눌러 중요 표시를 설정할 수 있다.
-6. 특정 탭을 클릭하면 해당 탭으로 이동한 뒤 목록을 표시한다. (전체, 중요, 완료, 미완료)
-*/
-
 let userInput = document.getElementById("user-input");
 let inputButton = document.getElementById("input-button");
 let checkButton = document.getElementById("check-button");
@@ -104,7 +95,6 @@ function checkTask(id) {
   for (i = 0; i < taskList.length; i++) {
     if (id == taskList[i].id) {
       taskList[i].isComplete = !taskList[i].isComplete;
-      console.log(taskList[i].isComplete);
     }
   }
   filtering();
@@ -122,7 +112,6 @@ function deleteTask(id) {
 function filtering(event) {
   if (event) {
     selectedTab = event.target.id;
-    console.log(selectedTab);
   }
 
   checkedList = [];
